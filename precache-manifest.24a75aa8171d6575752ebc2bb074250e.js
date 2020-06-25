5d60dd4068085db89965afc9eda43518a44bd9d8
rahul <kumarsrahul123@gmail.com>
2020-06-25 18:33:02 +0530

    Updates

---
+++ precache-manifest.24a75aa8171d6575752ebc2bb074250e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8888f772d946f90b8c16f036d9f9c857",
+    "url": "/fashionista-bot/index.html"
+  },
+  {
+    "revision": "64e6451d84b0449e5076",
+    "url": "/fashionista-bot/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9a7c05bbc64a8c5f3653",
+    "url": "/fashionista-bot/static/css/main.3e18473b.chunk.css"
+  },
+  {
+    "revision": "64e6451d84b0449e5076",
+    "url": "/fashionista-bot/static/js/2.de2fe070.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/fashionista-bot/static/js/2.de2fe070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a7c05bbc64a8c5f3653",
+    "url": "/fashionista-bot/static/js/main.997b439d.chunk.js"
+  },
+  {
+    "revision": "5d4a80b0807ef79494d2",
+    "url": "/fashionista-bot/static/js/runtime-main.29b5bd26.js"
+  }
+]);
